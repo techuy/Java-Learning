f331a2a926b99921c6ec5843397074086889446b
Tech Uy <lavtechuy18@kit.edu.kh>
2019-06-05 21:02:31 +0700

    chapter 3

--- Head First JAva/Chapter 3/Hobbits.java
+++ Head First JAva/Chapter 3/Hobbits.java
@@ -1,13 +1,12 @@
-class Hobbit{
-    String name;
-}
+
 class Hobbits{
+    String name;
     public static void main(String[] args) {
-        Hobbit[] h = new Hobbit[3];
+        Hobbits[] h = new Hobbits[3];
         int z=0;
         while (z<3) {
             
-            h[z]=new Hobbit();
+            h[z]=new Hobbits();
             h[z].name = "bilbo";
             if(z==1){
                 h[z].name="frodo";
